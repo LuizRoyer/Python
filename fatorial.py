4847149616dc444e743de170d232023df71e2725
LuizRoyer <32273858+LuizRoyer@users.noreply.github.com>
2024-08-15 10:09:28 -0300

    Add files via upload

---
+++ fatorial.py
@@ -0,0 +1,17 @@
+#recursividade
+
+def fatorial(number):
+	if number == 0 or number ==1:
+		return 1
+	return number * fatorial(number -1)
+
+
+if __name__ =='__main__':
+	
+	number = int(input('digite um numero: '))
+	try:
+		result = fatorial(number)
+	except RecursionError:
+		print('numero invalido')
+	else:
+		print(f'o Fatorial de {number} = {result}') # ou print(result)
